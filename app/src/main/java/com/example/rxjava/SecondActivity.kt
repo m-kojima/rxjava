df1f50cca1f4ff16e59c940829a612b5acd9114f
m-kojima <5977111+m-kojima@users.noreply.github.com>
2019-01-24 13:49:00 +0900

    関数オブジェクト(SecondActivityFunc)を定義した場合、破棄されている

--- app/src/main/java/com/example/rxjava/SecondActivity.kt
+++ app/src/main/java/com/example/rxjava/SecondActivity.kt
@@ -3,31 +3,35 @@ package com.example.rxjava
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.jakewharton.rxbinding2.widget.RxCompoundButton
-import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+    private val disposeBag = CompositeDisposable()
     private lateinit var viewModel: SecondViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+    }
 
-        this.viewModel = SecondViewModel()
+    class SecondActivityFunc: Consumer<Boolean> {
+        override fun accept(t: Boolean?) {
+            println(t.toString())
+        }
     }
 
     override fun onStart() {
         super.onStart()
 
         val switchChanged = RxCompoundButton.checkedChanges(activity2_switch).share()
-        switchChanged.filter { it }.map { Unit }.subscribe(this.viewModel.onStart)
-        switchChanged.filter { !it }.map { Unit }.subscribe(this.viewModel.onStop)
-
-        this.viewModel.elapsed.map { it.toString() }.subscribe(RxTextView.text(activity2_label))
+        this.disposeBag.add(switchChanged.subscribe(SecondActivityFunc()))
     }
 
     override fun onStop() {
         super.onStop()
-    }
 
+        this.disposeBag.clear()
+    }
 }
